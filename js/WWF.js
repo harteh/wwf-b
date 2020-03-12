1baf8d486e9eb71dd8c30506292d09e20654b3f2
harteh <harteh31@gmail.com>
2020-03-12 20:39:31 +0900

    통합수정

--- js/WWF.js
+++ js/WWF.js
@@ -20,7 +20,6 @@
 
             //세계 WWF End
 
-
             var i = 0,
                 d = 0,
                 time = false,
@@ -43,10 +42,11 @@
                 $slide.eq(i).stop().css("left", "100%").animate({
                     left: "0"
                 }, 1500)
+
+                $countBox.text(count[i])
+
                 d = i;
             }
-
-
 
             $arrow.click(function (e) {
                 e.preventDefault();
@@ -90,63 +90,65 @@
             //메인 슬라이더 루프 영역 End
 
             var a = 0,
-        leng = $("#sec5 .sec5-bottom .sec5-bottom-wrap .js-leng").length - 10,
-            // 넘어가서(슬라이드 되어서) 보여질 박스의 갯수
-        slideWrap = $("#sec5 .sec5-bottom .sec5-bottom-wrap .js-slideWrap"),
-            // 나열된 슬라이드 박스 틀 ← 이게 옆으로 100%씩 이동함.
-        slideBtn = $("#sec5 .sec5-bottom .sec5-bottom-wrap .slide-btn i"),
-        time = false;
-    
-    var noticeTitleBtn = $("#sec5 .sec5-bottom .sec5-bottom-wrap .notice-wrap .box i");
-    var asideUp = $('aside');
-        
-    slideBtn.click(function(e) {
-        e.preventDefault();
-        var i = slideBtn.index(this);
+                leng = $("#sec5 .sec5-bottom .sec5-bottom-wrap .js-leng").length - 10,
+                // 넘어가서(슬라이드 되어서) 보여질 박스의 갯수
+                slideWrap = $("#sec5 .sec5-bottom .sec5-bottom-wrap .js-slideWrap"),
+                // 나열된 슬라이드 박스 틀 ← 이게 옆으로 100%씩 이동함.
+                slideBtn = $("#sec5 .sec5-bottom .sec5-bottom-wrap .slide-btn img"),
+                time = false;
 
-        if (time == true) {
-            return;
-        }        
-        setTimeout(function() {
-            time = false;
-        }, 1000)
+            var noticeTitleBtn = $("#sec5 .sec5-bottom .sec5-bottom-wrap .notice-wrap .box img");
+            var asideUp = $('aside');
 
-        time = true;
+            slideBtn.click(function (e) {
+                e.preventDefault();
+                var i = slideBtn.index(this);
 
-        if (i == 0) {
-            // 왼쪽 화살표 ( < ) 클릭 시 설정
-            a--;
-            if(a < 0) a = leng;
-        } else {
-            // 오른쪽 화살표 ( > ) 클릭 시 설정
-            a++;
-            if(a > leng) a = 0;     // 마지막 슬라이드에서 첫 슬라이드로 돌아감.         
-        }
-        
-        slideWrap.stop().animate({
-            left: "-100" * a + "%"
-        }, 1000)
-        
-    })  // 슬라이드 설정 끝    
+                if (time == true) {
+                    return;
+                }
+                setTimeout(function () {
+                    time = false;
+                }, 1000)
 
-    noticeTitleBtn.click(function(){
-        $(this).next().slideToggle(500).siblings(".notice.content").slideUp(500);
-        $(this).toggleClass('js-rotate')
-    })  // 공지사항 밑으로 더 보기 버튼 설정
+                time = true;
 
-    $(window).scroll(function(){
+                if (i == 0) {
+                    // 왼쪽 화살표 ( < ) 클릭 시 설정
+                    a--;
+                    if (a < 0) a = leng;
+                } else {
+                    // 오른쪽 화살표 ( > ) 클릭 시 설정
+                    a++;
+                    if (a > leng) a = 0; // 마지막 슬라이드에서 첫 슬라이드로 돌아감.         
+                }
 
-        var height = $(document).scrollTop();
+                slideWrap.stop().animate({
+                    left: "-100" * a + "%"
+                }, 1000)
 
-        if(height > 500){
-            asideUp.show(500)
-        } else {
-            asideUp.hide(500)
-        }   // 어사이드 표시 설정
-    })   // 스크롤 이벤트
+            }) // 슬라이드 설정 끝    
 
-    asideUp.click(function(){
-        $('html, body').animate({scrollTop: 0}, 400);
-    })  // 어사이드 클릭 이벤트
+            noticeTitleBtn.click(function () {
+                $(this).next().slideToggle(500).siblings(".notice.content").slideUp(500);
+                $(this).toggleClass('js-rotate')
+            }) // 공지사항 밑으로 더 보기 버튼 설정
+
+            $(window).scroll(function () {
+
+                var height = $(document).scrollTop();
+
+                if (height > 500) {
+                    asideUp.show(500)
+                } else {
+                    asideUp.hide(500)
+                } // 어사이드 표시 설정
+            }) // 스크롤 이벤트
+
+            asideUp.click(function () {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 400);
+            }) // 어사이드 클릭 이벤트
 
         })
